66e9c07dc4c1a29048b794762e4dbeb63218333a
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-06-07 14:56:11 -0400

    Arregla error ortografico

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('CUE_APELL_PATERNO',60);
             $table->string('CUE_APELL_MATERNO',60);
             $table->string('CUE_TIPO',15);
-            $table->string('CUE_TELOFONO',40);
+            $table->string('CUE_TELEFONO', 40);
             $table->rememberToken();
         });
     }
