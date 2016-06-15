dc85c2212d803723d81cea2ae0d75edcdf54ca18
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-06-15 18:46:17 -0400

    Mustra campo de empresas con jquery

--- public/js/js formularios/form cuentas.js
+++ public/js/js formularios/form cuentas.js
@@ -4,4 +4,12 @@ $(function () {
             alert("Rut ingresado no valido")
         }
     });
+    $('input:radio[name="tipo"]').on('change', function () {
+        if ($(this).val() == 'cliente' || $(this).val() == 'ambos') {
+            $('#empresas').show();
+        }
+        else {
+            $('#empresas').hide();
+        }
+    });
 });
