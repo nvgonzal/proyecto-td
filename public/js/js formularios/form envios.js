7f61fe40014aebe4ea62628f4325f5d062a0ba3f
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-06-24 17:36:19 -0400

    JS para manejo de formulario de envios

---
+++ public/js/js formularios/form envios.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Nicolas on 0017, 17-06-2016.
+ */
+
+$(function () {
+    var direccionRecogida = $('#direccion_recogida');
+    var direccionDestino = $('#direccion_destino');
+
+    direccionRecogida.geocomplete();
+    direccionDestino.geocomplete();
+
+    $('.datepicker').datepicker({
+        format: "dd-mm-yyyy",
+        todayBtn: true,
+        language: "es",
+        todayHighlight: true
+    });
+});
