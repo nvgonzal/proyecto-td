156e003d15397088bbdaa966f13a40a6567cf276
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-06-29 15:22:13 -0400

    Agrega restriccion para tipos de usuario

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,11 +8,12 @@
 </head>
 <body>
 
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -24,41 +25,55 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    @if(Auth::check())
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gestion envios<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Ver envios activos</a></li>
-                        <li><a href="{{URL::to('/cliente/verhistorial')}}">Ver Historial de envios</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Creacion de envios</a></li>
-                    </ul>
+                @if(Auth::check())
+                    @if(Auth::user()->CUE_TIPO == 'ambos' || Auth::user()->CUE_TIPO == 'cliente')
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-haspopup="true" aria-expanded="false">Gestion envios<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Ver envios activos</a></li>
+                                <li><a href="{{URL::to('/cliente/verhistorial')}}">Ver Historial de envios</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="{{URL::to('cliente/envio/create')}}">Creacion de envios</a></li>
+                            </ul>
+                        </li>
                     @endif
-                </li>
+                    @if(Auth::user()->CUE_TIPO == 'ambos' || Auth::user()->CUE_TIPO == 'transportista')
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-haspopup="true" aria-expanded="false">Ver envios<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{URL::to('transportista/envios')}}">Envios en mi area</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                @endif
+
             </ul>
             <!--Busqueda-->
             @if(Auth::check())
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Cuenta</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false">Gestion de cuenta <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{URL::to('/edit')}}">Editar Informacion</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="{{URL::to('/logout')}}">Cerrar sesion</a></li>
-                    </ul>
-                </li>
-            </ul>
-                @else
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Cuenta</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Gestion de cuenta <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{URL::to('/edit')}}">Editar Informacion</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="{{URL::to('/logout')}}">Cerrar sesion</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            @else
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="{{URL::to('/register')}}">Crear cuenta</a></li>
                     <li><a href="{{URL::to('/login')}}">Iniciar sesion</a></li>
                 </ul>
             @endif
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
 </nav>
 
 
@@ -70,7 +85,7 @@
 
 
 {!! Html::script('js/jquery-1.12.4.js') !!}
-    {!! Html::script('js/bootstrap.js') !!}
-    @yield('js')
+{!! Html::script('js/bootstrap.js') !!}
+@yield('js')
 </body>
 </html>
