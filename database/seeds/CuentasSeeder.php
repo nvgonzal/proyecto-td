2dc5d56dcfe90182d54c222f2d7c6ff9be92f455
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-07-10 15:19:54 -0400

    Seeder para crear dos cuentas

--- database/seeds/CuentasSeeder.php
+++ database/seeds/CuentasSeeder.php
@@ -38,5 +38,38 @@ class CuentasSeeder extends Seeder
             'CUE_ID' => $cuenta1->CUE_ID,
             'TRA_VALORACION' => 0,
         ]);
+
+        $cuenta2 = Cuenta::create([
+            'CUE_EMAIL' => 'tra@foo.com',
+            'CUE_PASSWORD' => Hash::make('td12345'),
+            'CUE_NOMBRES' => $faker->firstName,
+            'CUE_APELL_PATERNO' => $faker->lastName,
+            'CUE_APELL_MATERNO' => $faker->lastName,
+            'CUE_TELEFONO' => $faker->phoneNumber,
+            'CUE_RUT' => '22.222.222-2',
+            'CUE_TIPO' => 'transportista',
+        ]);
+
+        Transportista::create([
+            'CUE_ID' => $cuenta2->CUE_ID,
+            'TRA_VALORACION' => 0,
+        ]);
+
+        $cuenta3 = Cuenta::create([
+            'CUE_EMAIL' => 'cli@foo.com',
+            'CUE_PASSWORD' => Hash::make('td12345'),
+            'CUE_NOMBRES' => $faker->firstName,
+            'CUE_APELL_PATERNO' => $faker->lastName,
+            'CUE_APELL_MATERNO' => $faker->lastName,
+            'CUE_TELEFONO' => $faker->phoneNumber,
+            'CUE_RUT' => '33.333.333-3',
+            'CUE_TIPO' => 'cliente',
+        ]);
+
+        Cliente::create([
+            'CUE_ID' => $cuenta3->CUE_ID,
+            'EMP_ID' => null,
+            'CLI_VALORACION' => 0,
+        ]);
     }
 }
