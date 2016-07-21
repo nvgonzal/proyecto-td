7c774da20076ddca745d40dea7a633052daa6b24
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-07-20 20:11:34 -0400

    Layout para vistas de error

---
+++ resources/views/error.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Error @yield('titulo')</title>
+    {!! Html::style('css/bootstrap.css') !!}
+    <style>
+        .error-template {
+            padding: 40px 15px;
+            text-align: center;
+        }
+
+        .error-actions {
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+
+        .error-actions .btn {
+            margin-right: 10px;
+        }
+    </style>
+</head>
+<body>
+@yield('contenido')
+</body>
+</html>
