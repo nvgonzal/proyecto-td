649f8f8e0995b4c855248569c6272ad61ff89267
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-07-20 20:11:52 -0400

    Vista que muestra los envios marcados como finalizados

---
+++ resources/views/envio/finalizados.blade.php
@@ -0,0 +1,54 @@
+@extends('master')
+
+@section('titulo','Envios finalizados')
+
+@section('contenido')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Historial de envios</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                <th>Descripcion</th>
+                                <th>Direccion de carga</th>
+                                <th>Direccion de destino</th>
+                                <th>Estado</th>
+                                <th>Detalles</th>
+                                <th>Evaluar</th>
+                                </thead>
+                                @foreach($envios as $envio)
+                                    <tr data-id="{{$envio->ENV_ID}}">
+                                        <td>{{$envio->ENV_DESCRIPCION}}</td>
+                                        <td>{{$envio->ENV_DIRECCION_RECOGIDA}}</td>
+                                        <td>{{$envio->ENV_DIRECCION_DESTINO}}</td>
+                                        <td>{{$envio->ENV_ESTADO}}</td>
+                                        <td>
+                                            <a class="btn btn-primary btn-sm" data-toggle="tooltip"
+                                               title="Informacion detallada"
+                                               href="{{URL::to('cliente/envio/'.$envio->ENV_ID)}}">
+                                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a id="boton-finalizar" class="btn btn-danger btn-sm" data-toggle="tooltip"
+                                               title="Evaluar"
+                                               href="#">
+                                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center"> {!! $envios->render() !!}</div>
+        </div>
+    </div>
+@stop
