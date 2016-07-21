a7e9a7c3906f6f081e76675e413e6e0108d3e2f7
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-07-20 20:07:16 -0400

    Crea vista para error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+@extends('error')
+
+@section('titulo','404 | Pagina no encontrada')
+
+@section('contenido')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-template">
+                    <h1>
+                        Oops!</h1>
+                    <h2>
+                        404</h2>
+                    <div class="error-details">
+                        ¡La pagina solicitada no se encontro!
+                    </div>
+                    <div class="error-actions">
+                        <a href="{{URL::to('home')}}" class="btn btn-primary"><span
+                                    class="glyphicon glyphicon-home"></span>
+                            Volver a inicio </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
