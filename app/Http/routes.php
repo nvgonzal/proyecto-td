e2a5e05ad9b4e6fbcc7778532c01506bfaf21c1b
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-07-26 20:52:21 -0400

    Agrega rutas nuevas y agrega middleware a las existentes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,21 +36,39 @@ Route::get('logout/delete', ['as' => 'auth.logout.delete', 'uses' => 'Auth\AuthC
 Route::post('logout/delete', ['as' => 'auth.logout.delete', 'uses' => 'Auth\AuthController@logoutAndDelete']);
 
 //Ruta para Editar la cuenta
-Route::get('edit',['as'=>'auth.edit','uses'=>'CuentasController@edit']);
-Route::put('edit/per',['as'=>'auth.edit.per','uses'=>'CuentasController@editInformacionPersonal']);
-Route::put('edit/pass',['as'=>'auth.edit.pass','uses'=>'CuentasController@editPassword']);
-Route::put('edit/cue',['as'=>'auth.edit.cue','uses'=>'CuentasController@editInformacionCuenta']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('edit', ['as' => 'auth.edit', 'uses' => 'CuentasController@edit']);
+    Route::put('edit/per', ['as' => 'auth.edit.per', 'uses' => 'CuentasController@editInformacionPersonal']);
+    Route::put('edit/pass', ['as' => 'auth.edit.pass', 'uses' => 'CuentasController@editPassword']);
+    Route::put('edit/cue', ['as' => 'auth.edit.cue', 'uses' => 'CuentasController@editInformacionCuenta']);
+    Route::put('edit/img', ['as' => 'auth.edit.img', 'uses' => 'CuentasController@editImagen']);
+});
 
-Route::group(['prefix' => 'transportista', 'middleware' => ['tran', 'auth']], function () {
+Route::group(['prefix' => 'transportista', 'middleware' => ['auth', 'tran']], function () {
     //Rutas para CRUD de vehiculo
     Route::get('mis-vehiculos/', ['as' => 'vehiculos', 'uses' => 'VehiculoController@index']);
     Route::get('mis-vehiculos/create', ['as' => 'vehiculos.create', 'uses' => 'VehiculoController@create']);
     Route::get('mis-vehiculos/{id}', ['as' => 'vehiculos.show', 'uses' => 'VehiculoController@show']);
 
     Route::get('envios', ['as' => 'envios', 'uses' => 'TransportistaController@verEnvios']);
-    Route::get('envios/reg/{id}', ['as' => 'envios.reg', 'uses' => 'TransportistaController@tomarEnvio']);
+    Route::get('envios/reg/{id}', [
+        'as' => 'envios.reg',
+        'uses' => 'TransportistaController@tomarEnvio',
+        'middleware' => ['deletable', 'takable']]);
     Route::get('envios/taken', ['as' => 'envios.taken', 'uses' => 'TransportistaController@verEnviosTomados']);
-    Route::get('envios/undo/{id}', ['as' => 'envios.undo', 'uses' => 'TransportistaController@cancelarSolicitud']);
+    Route::get('envios/undo/{id}', [
+        'as' => 'envios.undo',
+        'uses' => 'TransportistaController@cancelarSolicitud',
+        'middleware' => ['deletable', 'takable']]);
+
+    Route::get('envio/{id}/rate', ['as' => 'envio.rate.cli', 'uses' => 'TransportistaController@evaluar', 'middleware' => ['rateable', 'traev']]);
+    Route::put('envio/{id}/rate', [
+        'as' => 'envio.rate.cli',
+        'uses' => 'TransportistaController@registrarEvaluacion',
+        'middleware' => ['rateable', 'traev'],
+    ]);
+
+    Route::get('envios/fin', ['as' => 'envios.fin']);
 });
 
 Route::group(['prefix' => 'cliente', 'middleware' => ['auth', 'cli']], function () {
@@ -58,31 +76,39 @@ Route::group(['prefix' => 'cliente', 'middleware' => ['auth', 'cli']], function 
     Route::get('verhistorial',['as'=>'envio.verhistorial','uses'=>'EnvioController@index']);
     Route::get('envio/create/', ['as' => 'envio.create', 'uses' => 'EnvioController@create']);
     Route::post('envio/create/', ['as' => 'envio.store', 'uses' => 'EnvioController@store']);
-    Route::get('envio/edit/{id}', ['as' => 'envio.edit', 'uses' => 'EnvioController@edit', 'middleware' => 'envi']);
-    Route::put('envio/edit/{id}', ['as' => 'envio.edit', 'uses' => 'EnvioController@update', 'middleware' => 'envi']);
-    Route::get('envio/delete/{id}', ['as' => 'envio.delete', 'uses' => 'EnvioController@destroy', 'middleware' => 'envi']);
+    Route::get('envio/edit/{id}', ['as' => 'envio.edit', 'uses' => 'EnvioController@edit', 'middleware' => ['envi', 'editable']]);
+    Route::put('envio/edit/{id}', ['as' => 'envio.edit', 'uses' => 'EnvioController@update', 'middleware' => ['envi', 'editable']]);
+    Route::get('envio/delete/{id}', [
+        'as' => 'envio.delete',
+        'uses' => 'EnvioController@destroy',
+        'middleware' => ['envi', 'deletable']]);
 
     Route::get('verhistorial/asignados', ['as' => 'envio.asignados', 'uses' => 'EnvioController@enviosAsignados']);
     Route::get('verhistorial/activos', ['as' => 'envio.activos', 'uses' => 'EnvioController@enviosActivos']);
     Route::get('verhistorial/finalizados', ['as' => 'envio.fin', 'uses' => 'EnvioController@enviosFinalizados']);
 
-    Route::get('envio/solicitudes/{id}',
-        ['as' => 'envio.solicitudes',
-            'uses' => 'EnvioController@verSolicitudes',
-            'middleware' => 'envi']);
+    Route::get('envio/solicitudes/{id}', [
+        'as' => 'envio.solicitudes',
+        'uses' => 'EnvioController@verSolicitudes',
+        'middleware' => 'envi']);
 
     Route::get('envio/solicitudes/{id}/acep/{tra}',
-        ['as' => 'envio.aceptar',
-            'uses' => 'EnvioController@aceptarSolicitud',
-            'middleware' => 'envi']);
+        ['as' => 'envio.aceptar', 'uses' => 'EnvioController@aceptarSolicitud', 'middleware' => 'envi']);
 
     Route::get('envio/finalizar/{id}', ['as' => 'envio.finalizar', 'uses' => 'EnvioController@finalizarEnvio',
         'middleware' => 'envi']);
+
+    Route::get('envio/{id}/rate', ['as' => 'envio.rate.tra', 'uses' => 'EnvioController@evaluar', 'middleware' => ['envi', 'rateable']]);
+    Route::put('envio/{id}/rate', [
+        'as' => 'envio.rate.tra',
+        'uses' => 'EnvioController@registrarEvaluacion',
+        'middleware' => ['envi', 'rateable']]);
 });
 
 Route::get('cliente/envio/{id}', ['as' => 'envio.show', 'uses' => 'EnvioController@show']);
 
 Route::get('cuenta/info/{id}', ['as' => 'cuenta.info', 'uses' => 'CuentasController@show']);
 Route::get('cuentas', ['as' => 'cuentas', 'uses' => 'CuentasController@index']);
+
 
 Route::get('empresa/{id}', ['as' => 'empresa.show', 'uses' => 'EmpresaController@show']);
