7bc64376aece85c852bfaade57d4617daa8c6b73
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-07-26 20:46:48 -0400

    Vista para error 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,29 @@
+@extends('error')
+
+@section('titulo','403 | No autorizado')
+
+@section('contenido')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-template">
+                    <h1>
+                        Oops!</h1>
+                    <h2>
+                        403</h2>
+                    <div class="error-details">
+                        ¡No tienes permisos para realizar esta accion!
+                    </div>
+                    <div class="error-actions">
+                        <a href="{{URL::to('/')}}" class="btn btn-primary"><span
+                                    class="glyphicon glyphicon-home"></span>
+                            Volver a inicio </a>
+                        <a href="{{URL::previous()}}" class="btn btn-primary"><span
+                                    class="glyphicon glyphicon-arrow-left"></span>
+                            Volver atras </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
