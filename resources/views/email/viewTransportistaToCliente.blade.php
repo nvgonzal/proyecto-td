d4cfcde79013c211950a20389656990886b09f40
Franco Tardones Peña <ftardones@gmail.com>
2016-07-27 13:31:40 -0300

    Se creo vista para el mail cuando el transportista postule a un envio

---
+++ resources/views/email/viewTransportistaToCliente.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Saludos</h4>
+    <p>Tu envio con identificador <strong>{!! $Id_Envio !!}</strong> a sido tomado por <strong>{!! $Nombre !!}</strong></p>
+    <p>Ponte en contacto con él, por medio de su correo <strong>{!! $Correo !!}</strong> o su telefono <strong>{!! $Telefono !!}</strong></p>
+    <p>Revisa su perfil haciendo <a href="http://localhost/proyecto_td/public/index.php/cuenta/info/{!! $Id_Tra !!}">click aqui</a></p>
+</body>
+</html>
